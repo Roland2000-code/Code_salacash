d99743322109daea477ef294ccb0ac6470aff110
Roland <rolandgold38@gmail.com>
2024-07-26 10:50:13 +0200

    connexion base des données

---
+++ db_config.php
@@ -0,0 +1,5 @@
+<?php
+$host = "localhost"; // Replace with your database hostname
+$dbname = "salacash_bd"; // Replace with your database name
+$username = "root"; // Replace with your database username
+$password = ""; // Replace with your database password
